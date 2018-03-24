8cc7dcdd589effb9da3e438dc5db38555c25558c
nicholas <nick.mancuso@gmail.com>
2018-03-23 22:29:22 -0700

    updated main function

--- main.py
+++ main.py
@@ -11,13 +11,15 @@ import scipy.stats as stats
 from numpy.linalg import multi_dot
 
 
+VANILLA = "vanilla"
+POLYAK = "polyak"
 
 def quadratic(size=2):
     Q = stats.wishart.rvs(size ** 2, np.eye(size))
     b = stats.norm.rvs(size=size)
     c = stats.norm.rvs()
-    f = lambda x: multi_dot([x, Q, x]) + b.dot(x) + c
-    g = lambda x: Q.dot(x) + b
+    f = lambda x: multi_dot([x, Q, x]) - b.dot(x) + c
+    g = lambda x: Q.dot(x) - b
 
     evals = linalg.eigvals(Q)
     beta = max(evals)
@@ -26,31 +28,56 @@ def quadratic(size=2):
     return (f, g, beta, alpha)
 
 
+def run(optfun, f, x0, step, grad, verbose, error_tol, output, **kwargs):
+    for idx, local in enumerate(optfun(x0, step, grad, **kwargs)):
+        lsol_cur = f(local)
+        if verbose:
+            output.write("Step {}: f({}) = {}{}".format(idx, local, f(local), os.linesep))
+        else:
+            output.write("\rStep {}: f(xi) = {}".format(idx, f(local)))
+            output.flush()
+
+        if idx > 0 and lsol_last - lsol_cur < error_tol:
+            break
+
+        lsol_last = lsol_cur
+
+    if not verbose:
+        output.write(os.linesep)
+
+    return
+
+
 def main(args):
     argp = ap.ArgumentParser(description="")
     argp.add_argument("-d", "--dim", type=int, default=2)
     argp.add_argument("-l", "--learn_rate", type=float, default=0.1)
     argp.add_argument("-e", "--error-tol", type=float, default=1e-3)
-    argp.add_argument("--opt-rate", action="store_true", default=False, help="Use optimal learning rate?")
+    argp.add_argument("-r", "--opt_rate", action="store_true", default=False)
+    argp.add_argument("-v", "--verbose", action="store_true", default=False)
     argp.add_argument("-o", "--output", type=ap.FileType("w"), default=sys.stdout)
 
     args = argp.parse_args(args)
 
+    # build function
     f, g, beta, alpha = quadratic(args.dim)
+
+    # use optimal step size?
     if args.opt_rate:
         step_size = 1 / beta
     else:
         step_size = args.learn_rate
 
+    # initial guess
     x0 = np.random.normal(size=args.dim)
-    for idx, local in enumerate(opt.gradient_descent(x0, step_size, g)):
-        lsol_cur = f(local)
-        args.output.write("Step {}: f({}) = {}{}".format(idx, local, f(local), os.linesep))
 
-        if idx > 0 and lsol_last - lsol_cur < args.error_tol:
-            break
+    #GD
+    args.output.write("Vanilla Gradient Descent" + os.linesep)
+    run(opt.gd, f, x0, step_size, g, args.verbose, args.error_tol, args.output)
 
-        lsol_last = lsol_cur
+    #GD + momentum
+    args.output.write("Polyak Heavy-ball Gradient Descent" + os.linesep)
+    run(opt.mgd, f, x0, step_size, g, args.verbose, args.error_tol, args.output, friction=0.25)
 
     return 0
 
--- opt/grad.py
+++ opt/grad.py
@@ -1,5 +1,5 @@
 
-def gradient_descent(x0, step, grad):
+def gd(x0, step, grad, **kwargs):
     """ Vanilla gradient descent
 
     Params
@@ -23,5 +23,38 @@ def gradient_descent(x0, step, grad):
     while True:
         xi = xi - step * grad(xi)
         yield xi
+
+    return
+
+
+def mgd(x0, step, grad, **kwargs):
+    """ Polyak's heavy ball method for gradient descent
+
+    Params
+    ------
+
+    x0 : initial guess
+
+    step : step size (ie learning rate)
+
+    grad : gradient function
+
+    Yields
+    ------
+
+    xi : next step along gradient direction
+    """
+
+    yield x0
+
+    friction = kwargs.get("friction", 0)
+
+    xlast = x0
+    xi = xlast
+    while True:
+        tmp = xi - step * grad(xi) + friction * (xi - xlast)
+        yield tmp
+        xlast = xi
+        xi = tmp
 
     return
